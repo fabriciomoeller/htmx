8a3294ff136d9429851dd584884213e91d6f69ce
fabriciomoeller <fabriciomoeller@gmail.com>
2024-07-07 15:05:57 -0300

    Criado modelo htmx

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := struct {
+			Message string
+		}{
+			Message: "Hello World",
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Current time: " + time.Now().Format(time.RFC1123)))
+	})
+
+	fmt.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", nil)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fabriciomoeller/htmx
+
+go 1.22.1
