d0a532e364dfaad869c4642858d68fa57326d88c
fabriciomoeller <fabriciomoeller@gmail.com>
2024-07-07 22:47:42 -0300

    Criado index.html e login com tailwindcss

--- cmd/main.go
+++ cmd/main.go
@@ -16,9 +16,14 @@ func main() {
 		}
 
 		data := struct {
-			Message string
+			Login      string
+			Senha      string
+			urlDoc     string
+			urlSuporte string
 		}{
-			Message: "Hello World",
+			Login:      "login",
+			urlDoc:     "http://gurusistemas.com.br",
+			urlSuporte: "http://gurubi.ddns.net:8070/",
 		}
 
 		err = tmpl.Execute(w, data)
@@ -30,6 +35,22 @@ func main() {
 
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Current time: " + time.Now().Format(time.RFC1123)))
+	})
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("login.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := struct {
+			Login string
+		}{
+			Login: "login",
+		}
+
+		_ = tmpl.Execute(w, data)
 	})
 
 	fmt.Println("Listening on port 8080")
